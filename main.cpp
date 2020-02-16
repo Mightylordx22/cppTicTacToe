78148cbb72a1cdb40e06c97a43ab2094d1e32279
Mightylordx <mightylordx786@gmail.com>
2020-02-16 01:53:00 +0000

    Updated code to fit the specification

--- main.cpp
+++ main.cpp
@@ -1,24 +1,36 @@
+// <- Includes ->
+
 #include <iostream>
 #include <string>
+#include <cstdlib>
+
+// <- Global Variables ->
 
 int currentPlayer = 0;
 char board[3][3] = {{'-', '-', '-'},
                     {'-', '-', '-'},
                     {'-', '-', '-'}};
 std::string playerOneName;
-std::string playerTwoName;
 bool playerWin = false;
 
-void getPlayerNames() {
-    std::cout << "Please enter a name: " << std::endl;
+// <- Functions ->
+
+/**
+ * Asks for the player's name and stores it in the global variable playerOneName.
+ */
+void getPlayerName() {
+    std::cout << "Welcome to my game of Tic Tac Toe! (You are 'X') " << std::endl << "Please enter your name: "
+              << std::endl;
     std::cin >> playerOneName;
-    std::cout << "Please enter a name for player two: " << std::endl;
-    std::cin >> playerTwoName;
 }
 
+/**
+ * Checks all the possible combinations on the board to see if either the player or the computer has won.
+ * @return true if any combinations are met else return false
+ */
 bool checkLine() {
     char playerChar;
-    if (currentPlayer == 0) {
+    if (currentPlayer % 2 == 0) {
         playerChar = 'X';
     } else {
         playerChar = 'O';
@@ -33,6 +45,12 @@ bool checkLine() {
     else return board[0][2] == playerChar && board[1][1] == playerChar && board[2][0] == playerChar;
 }
 
+/**
+ * Validate the position the player wants to play by checking the character in the position passed in.
+ * @param xPos The X position the current player has chosen.
+ * @param yPos The Y position the current player has chosen.
+ * @return
+ */
 bool validatePos(int xPos, int yPos) {
     if (xPos >= 0 && xPos < 3 && yPos >= 0 && yPos < 3) {
         if (board[xPos][yPos] != '-') {
@@ -41,28 +59,49 @@ bool validatePos(int xPos, int yPos) {
             return true;
         }
     } else {
-        std::cout << "Sorry those co-ordinates aren't valid" << std::endl;
+        std::cout << "Sorry those co-ordinates aren't valid! Please try again!" << std::endl;
     }
     return false;
 }
 
-void boardChoice() {
+/**
+ * Prints the board to the console and formats it so that it looks nice.
+ * The board should look like the following by default:
+ * +---+---+---+
+ * | - | - | - |
+ * +---+---+---+
+ * | - | - | - |
+ * +---+---+---+
+ * | - | - | - |
+ * +---+---+---+
+ */
+void printBoard() {
+    std::cout << "      [0]   [1]   [2]\n    +-----+-----+-----+" << std::endl;
+    for (int x = 0; x < 3; x++) {
+        std::cout << "[" << x << "] |  ";
+        for (int y = 0; y < 3; y++) {
+            std::cout << board[x][y] << "  |  ";
+        }
+        std::cout << "\n    +-----+-----+-----+" << std::endl;
+    }
+}
+
+/**
+ * The main function that gets the input from the user and then calls the other functions to validate and update the board.
+ * If the currentPlayer variable is even then it's the users's turn otherwise it's the computer's turn.
+ */
+void playGame() {
+    getPlayerName();
     while (true) {
         int xPos;
         int yPos;
         while (true) {
-            for (auto &x : board) {
-                for (char y : x) {
-                    std::cout << y;
-                }
-                std::cout << std::endl;
-            }
-            std::cout << std::endl;
-            if (currentPlayer == 0) {
-                std::cout << "It's Player One's turn!" << std::endl;
-                std::cout << "Please enter the x you want: " << std::endl;
+            printBoard();
+            if (currentPlayer % 2 == 0) {
+                std::cout << "\nIt's " << playerOneName << "'s turn!" << std::endl;
+                std::cout << "Please enter the x coord you want: " << std::endl;
                 std::cin >> xPos;
-                std::cout << "Please enter the y you want: " << std::endl;
+                std::cout << "Please enter the y coord you want: " << std::endl;
                 std::cin >> yPos;
                 if (!validatePos(xPos, yPos)) {
                     continue;
@@ -71,15 +110,13 @@ void boardChoice() {
                     if (checkLine()) {
                         playerWin = true;
                     }
-                    currentPlayer++;
                     break;
                 }
             } else {
-                std::cout << "It's Player Twos's turn!" << std::endl;
-                std::cout << "Please enter the x you want: " << std::endl;
-                std::cin >> xPos;
-                std::cout << "Please enter the y you want: " << std::endl;
-                std::cin >> yPos;
+                xPos = rand() % 3;
+                yPos = rand() % 3;
+                std::cout << "\nIt's the computer's turn now and they chose position: [" << xPos << " , " << yPos
+                          << "]" << std::endl << std::endl;
                 if (!validatePos(xPos, yPos)) {
                     continue;
                 } else {
@@ -87,18 +124,26 @@ void boardChoice() {
                     if (checkLine()) {
                         playerWin = true;
                     }
-                    currentPlayer--;
                     break;
                 }
             }
         }
         if (playerWin) {
+            printBoard();
+            if (currentPlayer % 2 == 0) {
+                std::cout << "\nThe game is now Over! " << playerOneName << " Wins!" << std::endl;
+            } else {
+                std::cout << "\nThe computer has won! Better luck next time :(" << std::endl;
+            }
             break;
         }
+        currentPlayer++;
     }
 }
 
+// <- Main Program ->
+
 int main() {
-    boardChoice();
+    playGame();
     return 0;
 }
